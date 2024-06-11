8526742663ca47e8d8935dfd4144c5319247a9f4
Dawood945 <151716734+Dawood945@users.noreply.github.com>
2024-06-11 17:00:56 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
+import sklearn
 import pickle
 
 # Load the model and label encoders
